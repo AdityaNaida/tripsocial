1273eda6c1c33943d4616e6499ee5ffa1d308e82
AdityaNaida <adityanaida@gmail.com>
2024-06-09 20:32:47 +0530

    Bug Fix Karwade

--- index.js
+++ index.js
@@ -10,75 +10,53 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 
-//all posts
-
 let posts = [
-  {
-    id: uuidv4(),
-    username: "Aditya",
-    caption:
-      "Feeling energized, inspired, and maybe a little bit in love with those giant slices of pizza. ",
-    img: "https://images.pexels.com/photos/472037/pexels-photo-472037.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-  },
-  {
-    id: uuidv4(),
-    username: "Mahi",
-    caption:
-      "Where history whispers on the wind.   Exploring ancient castles, strolling through charming gardens. ",
-    img: "https://images.pexels.com/photos/2682676/pexels-photo-2682676.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-  },
-  {
-    id: uuidv4(),
-    username: "Karan",
-    caption:
-      "Soaked up the sunshine, swam in crystal-clear coves, and sailed under the Sydney Harbour Bridge.",
-    img: "https://images.pexels.com/photos/1680247/pexels-photo-1680247.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-  },
+  // your initial posts
 ];
 
 app.get("/posts", (req, res) => {
-  res.render("index.ejs", { posts });
+  res.render("index", { posts });
 });
 
 app.post("/posts", (req, res) => {
-  const id = uuidv4();
   const { username, caption, img } = req.body;
+  const id = uuidv4();
   posts.push({ id, username, caption, img });
   res.redirect("/posts");
 });
 
 app.get("/posts/create", (req, res) => {
-  res.render("create.ejs");
+  res.render("create");
 });
 
 app.get("/posts/:id", (req, res) => {
   const { id } = req.params;
   const post = posts.find((p) => p.id === id);
-  res.render("show.ejs", { post });
+  res.render("show", { post });
 });
 
 app.get("/posts/:id/edit", (req, res) => {
   const { id } = req.params;
   const post = posts.find((p) => p.id === id);
-  res.render("edit.ejs", { post });
+  res.render("edit", { post });
 });
 
 app.patch("/posts/:id", (req, res) => {
-    const { id } = req.params;
-    const reqImg = req.body.img;
-    const reqCaption = req.body.caption;
-    const post = posts.find((p) => p.id === id);
-    post.img = reqImg;
-    post.caption = reqCaption;
-    res.redirect("/posts")
-})
+  const { id } = req.params;
+  const { img, caption } = req.body;
+  const post = posts.find((p) => p.id === id);
+  post.img = img;
+  post.caption = caption;
+  res.redirect("/posts");
+});
 
 app.delete("/posts/:id", (req, res) => {
-    const { id } = req.params;
-    posts = posts.filter((p) => p.id !== id);
-    res.redirect("/posts")
-})
+  const { id } = req.params;
+  posts = posts.filter((p) => p.id !== id);
+  res.redirect("/posts");
+});
 
-app.listen(8080, () => {
-  console.log(`listening at port: 8080`);
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+  console.log(`listening at port: ${port}`);
 });
